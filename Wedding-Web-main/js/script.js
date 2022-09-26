cb2feb45649567d8261130748997b21582660fe9
AhmadHidayat120402 <98697740+AhmadHidayat120402@users.noreply.github.com>
2022-09-26 20:12:13 +0700

    projek wedding invitation

---
+++ Wedding-Web-main/js/script.js
@@ -0,0 +1,24 @@
+/*==================== Animasi Kirim Hadiah &  Kado ====================*/
+const hadiah = document.getElementById('hadiah');
+const kado = document.getElementById('kado');
+const kirimHadiah = document.getElementById('kirimHadiah');
+const kirimKado = document.getElementById('kirimKado');
+
+
+hadiah.addEventListener('click', () => {
+    console.info('berhasil');
+    kirimHadiah.classList.add('active');
+    kirimKado.classList.remove('active')
+})
+
+
+kado.addEventListener('click', () => {
+    console.info('berhasil');
+    kirimKado.classList.add('active');
+    kirimHadiah.classList.remove('active')
+})
+
+/*==================== Animasi Kirim Hadiah &  Kado ====================*/
+
+const page = document.querySelector('#section');
+
